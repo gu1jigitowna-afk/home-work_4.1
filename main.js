3a156a48461d80178ebdd0c752b5d735f44128af
Твое имя <gu1jigitowna@gmail.com>
2026-05-21 15:34:52 +0600

    fix

---
+++ main.js
@@ -0,0 +1,24 @@
+const getActiveEmployees = (users) => {
+  if (users.length === 0) {
+    return "Список пуст";
+  }
+
+  const activeUsers = users.filter(
+    (user) => user.age >= 18 && user.isActive === true
+  );
+
+  return activeUsers.map((user) => user.name);
+};
+
+const EMPLOYEES_LIST = [
+  { id: 1, name: "Alin", age: 25, isActive: false },
+  { id: 2, name: "Amina", age: 30, isActive: true },
+  { id: 3, name: "Bek", age: 16, isActive: false },
+  { id: 4, name: "Syrgaim", age: 19, isActive: true }
+];
+
+const activeNames = getActiveEmployees(EMPLOYEES_LIST);
+console.log("Допущены к работе:", activeNames);
+
+const emptyResult = getActiveEmployees([]);
+console.log("Результат для пустого списка:", emptyResult);
